c4d0f7d388b6e64ec8eb9aa5af23f1eeaac3be8f
CharlesAverill <charlesaverill20@gmail.com>
2020-08-29 00:15:58 -0500

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+      transform.position += move * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+
+    public List<string> instructions;
+    public GameObject instructionObject;
+    public Transform instructionListSpawnPoint;
+    public RectTransform content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      instructions = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void read(){
+      Debug.Log("read");
+      addInstructionToList("Read");
+    }
+
+    public void write(){
+      Debug.Log("write");
+      addInstructionToList("Write");
+    }
+
+    public void left(){
+      Debug.Log("left");
+      addInstructionToList("Left");
+    }
+
+    public void right(){
+      Debug.Log("right");
+      addInstructionToList("Right");
+    }
+
+    public void addInstructionToList(string instruction){
+      instructions.Add(instruction);
+
+      content.sizeDelta = new Vector2(0, 60 * instructions.Count);
+
+      float spawnY = -1 * (490 - (60 * instructions.Count));
+      float spawnX = 0f;
+      Vector3 pos = new Vector3(spawnX, -spawnY, instructionListSpawnPoint.position.z);
+      GameObject spawnedInstruction = (GameObject)Instantiate(instructionObject, pos, instructionListSpawnPoint.rotation);
+      spawnedInstruction.transform.SetParent(instructionListSpawnPoint, false);
+
+      Instruction instructionComponent = spawnedInstruction.GetComponent<Instruction>();
+      instructionComponent.text.text = instruction;
+      switch(instruction){
+        case "Read":
+          instructionComponent.image.color = new Color32(250, 140, 22, 100);
+          break;
+        case "Write":
+          instructionComponent.image.color = new Color32(163, 250, 72, 100);
+          break;
+        case "Left":
+          instructionComponent.image.color = new Color32(22, 189, 250, 100);
+          break;
+        case "Right":
+          instructionComponent.image.color = new Color32(223, 35, 250, 100);
+          break;
+      }
+    }
+}
